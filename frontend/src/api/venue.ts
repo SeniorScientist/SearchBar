e94d719d267da57fee08a02e107b50b6afa66b77
SeniorScientist <f.nowak.w@gmail.com>
2023-11-20 17:59:30 +0100

    fix: modify venue api module

--- frontend/src/api/venue.ts
+++ frontend/src/api/venue.ts
@@ -2,8 +2,8 @@ import { IVenue } from '@/interfaces'
 import request from './request'
 
 const Venue = {
-  getChatsByOwner: (query: string) =>
-  request.get<IVenue[]>(`/autocomplete/query=${query}`),
+  getAutoComplete: (query: string) =>
+    request.get<IVenue[]>(`/autocomplete/query=${query}`),
 }
 
 export default Venue
